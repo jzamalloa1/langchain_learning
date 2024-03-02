a4e37905ba751cf0a9d7dbefc8f30b6ba117307c
JoseZ <jzamalloa1@gmail.com>
2024-03-02 17:22:06 -0500

    adding agent structure with blank tool. need to incorporate memory

--- agents_gpt_app.py
+++ agents_gpt_app.py
@@ -7,9 +7,11 @@ import numpy as np
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
 from langchain_core.runnables import RunnableLambda, RunnablePassthrough
+from langchain_core.utils.function_calling import convert_to_openai_tool
 from langchain.tools.retriever import create_retriever_tool
-from langchain.agents import create_openai_functions_agent, AgentExecutor
-
+from langchain.output_parsers import JsonOutputToolsParser, JsonOutputKeyToolsParser
+from langchain.agents import create_openai_tools_agent, AgentExecutor
+from langchain import hub
 
 st.title("Agent Streamer :linked_paperclips:") # More icons at https://streamlit-emoji-shortcodes-streamlit-app-gwckff.streamlit.app/
 
@@ -29,7 +31,14 @@ def main():
         
         # if "vector_store" not in st.session_state:
         #     st.session_state.vector_store
-            
+
+        # Initialize prompt and tools in session state
+        if "prompt" not in st.session_state:
+            st.session_state.prompt = hub.pull("hwchase17/openai-tools-agent")
+
+        if "tools" not in st.session_state:
+            st.session_state.tools = get_tool_box()
+
         # Initialize LLM model
         llm = ChatOpenAI(model=st.session_state["openai_model"], 
                         temperature=0.1, streaming=True, api_key=openai_api_key)
@@ -57,11 +66,36 @@ def main():
                 HumanMessage(content=prompt)
                 )
             
-            # Get Anget to work....
+            # Get Agent to work....
+            agent_response_object = get_ai_reponse(llm_model = llm, 
+                                                   chat_history = st.session_state.chat_history,
+                                                   prompt = st.session_state.prompt,
+                                                   tool_box = st.session_state.tool_box)
 
     else:
         st.warning("Please enter an Open API Key to get started")
 
+def get_ai_response(llm_model, chat_history, prompt, tool_box):
+
+    # Create agent
+    agent = create_openai_tools_agent(llm_model, tool_box, prompt)
+
+    # Create agent executor
+    agent_executor = AgentExecutor(agent=agent,
+                                   tools=tool_box,
+                                   verbose=True)
+
+    # Interact with agent and get agent's response
+    agent_output = agent_executor.invoke({
+        "input":""
+    })
+
+    return agent_output
+
+def get_tool_box():
+    tools = []
+
+    return tools
 
 if __name__ == "__main__":
 
